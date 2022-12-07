b1662cf1e0decba4ff1cf705eef5fb6e485a9191
Brian Chang <viere0928@gmail.com>
2022-12-07 13:49:07 +0800

    Initial project

---
+++ OOTD/OOTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOTDApp.swift
+//  OOTD
+//
+//  Created by Brian Chang on 2022/12/7.
+//
+
+import SwiftUI
+
+@main
+struct OOTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
